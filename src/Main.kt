8d6333338f677426f6e115f5d899398ba0e77152
collin4210 <collinsasuke@gmail.com>
2025-01-08 16:53:56 +0100

    Initial Commit

---
+++ src/Main.kt
@@ -0,0 +1,85 @@
+import java.time.LocalDate
+import java.time.Period
+import java.util.Date
+
+fun main() {
+
+    println("")
+
+
+}
+
+class User(var username: String, var password: String, var adress: String, var dateofBirth: String) {
+
+    private var shoppingCart: MutableList<String> = TODO()
+
+    fun addToCart() { //Alter des Benutzers muss über der Altersgrenze sein.
+
+        TODO()
+
+    }
+
+    fun removeFromCart() {
+
+        TODO()
+    }
+
+
+}
+
+class book(private var title: String, private var ageRestriction: String, private var releaseDate: String) {
+    fun getTitle() : String {
+
+        return title
+    }
+
+    fun getAgeRestriction() : String {
+
+        return ageRestriction
+
+    }
+
+    fun getReleaseDate() : String {
+
+        return releaseDate
+    }
+
+}
+
+class movie(private var title: String, private var ageRestriction: String, private var releaseDate: String) {
+    fun getTitle() : String {
+
+        return title
+    }
+
+    fun getAgeRestriction() : String {
+
+        return ageRestriction
+
+    }
+
+    fun getReleaseDate() : String {
+
+        return releaseDate
+    }
+
+}
+
+class merchandise(private var title: String, private var ageRestriction: String, private var releaseDate: String) {
+    fun getTitle() : String {
+
+        return title
+    }
+
+    fun getAgeRestriction() : String {
+
+        return ageRestriction
+
+    }
+
+    fun getReleaseDate() : String {
+
+        return releaseDate
+    }
+
+}
